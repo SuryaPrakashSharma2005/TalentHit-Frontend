2f42652484ac0558520e6c75ec3b4ad26c666728
surya prakash <sharmasuryaprakash1234@gmail.com>
2026-04-06 02:15:13 +0530

    add google oauth

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ErrorBoundary } from "./components/system/ErrorBoundary";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById("root")!).render(
-  <ErrorBoundary>
-    <App />
-  </ErrorBoundary>
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </GoogleOAuthProvider>
 );
